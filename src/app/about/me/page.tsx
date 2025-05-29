9918da5eadbac6dab87343e34dc27c268238f264
Denis Santos <denisdiogo34@gmail.com>
2025-05-29 09:30:32 -0300

    Sobre o taiwind CSS, clsx, e extensões do VS CODE para facilitar sua vida.

--- src/app/about/me/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function AboutMepage() {
-  return <h1>Hello World Me!</h1>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import clsx from 'clsx';
+
 export default function Homepage() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <h1
+        className={clsx(
+          'text-6xl',
+          'font-bold',
+          'text-blue-500',
+          'hover:text-white',
+          'hover:bg-blue-500',
+          'transition',
+          'duration-300',
+          'text-blog',
+        )}
+      >
+        Texto no meu h1
+      </h1>
+    </div>
+  );
 }
