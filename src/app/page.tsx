847893f91ebad88e621bb3420996ee76871d0a18
Denis Santos <denisdiogo34@gmail.com>
2025-07-02 09:40:37 -0300

    Cuidado para não mudar um Server Component em Client Component sem querer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 //Quando for usar componentes clients procurar jogar o mais para fora possivel da sua aplicação
 //page.tsx (server) -> menu.tsx (server) -> link (client)
 
+import { ClientComponent } from '@/components/ClientComponent';
 import { PostFeatured } from '@/components/PostFeatured';
 import { PostsList } from '@/components/PostsList';
+import { ServerComponent } from '@/components/ServerComponent';
+
 import { SpinLoader } from '@/components/SpinLoader';
 import clsx from 'clsx';
 import { Suspense } from 'react';
@@ -16,6 +19,9 @@ export default async function Homepage() {
   //throw new Error('Erro de teste na página inicial');
   return (
     <>
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
       <Suspense fallback={<SpinLoader className={clsx('min-h-20 mb-16')} />}>
         <PostFeatured />
         <PostsList />
---
+++ src/components/ServerComponent/index.tsx
@@ -0,0 +1,4 @@
+export function ServerComponent() {
+  console.log('Server Component Rendered');
+  return <div>Sever Component</div>;
+}
