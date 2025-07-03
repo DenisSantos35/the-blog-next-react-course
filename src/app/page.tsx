375de2055699c03e359abb37f10019c8c74b7f0d
Denis Santos <denisdiogo34@gmail.com>
2025-07-03 09:00:55 -0300

    Static Site Generation (SSG) no Next.js no App Router (export em HTML, CSS e JS)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 //Quando for usar componentes clients procurar jogar o mais para fora possivel da sua aplicação
 //page.tsx (server) -> menu.tsx (server) -> link (client)
 
-import { ClientComponent } from '@/components/ClientComponent';
 import { PostFeatured } from '@/components/PostFeatured';
 import { PostsList } from '@/components/PostsList';
-import { ServerComponent } from '@/components/ServerComponent';
-
 import { SpinLoader } from '@/components/SpinLoader';
 import clsx from 'clsx';
 import { Suspense } from 'react';
@@ -18,14 +15,9 @@ import { Suspense } from 'react';
 export default async function Homepage() {
   //throw new Error('Erro de teste na página inicial');
   return (
-    <>
-      <ClientComponent>
-        <ServerComponent />
-      </ClientComponent>
-      <Suspense fallback={<SpinLoader className={clsx('min-h-20 mb-16')} />}>
-        <PostFeatured />
-        <PostsList />
-      </Suspense>
-    </>
+    <Suspense fallback={<SpinLoader className={clsx('min-h-20 mb-16')} />}>
+      <PostFeatured />
+      <PostsList />
+    </Suspense>
   );
 }
