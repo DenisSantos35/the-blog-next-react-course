0d8a026800ee369e00e5e7f2467f4eed502b185f
Denis Santos <denisdiogo34@gmail.com>
2025-07-15 09:51:53 -0300

    Continuando a criação do repositório DrizzlePostRepository

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -4,6 +4,7 @@ import { drizzleDb } from '@/db/drizzle';
 
 export class DrizzePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
+    console.log('D findAllPublic');
     const posts = drizzleDb.query.posts.findMany({
       orderBy: (posts, { desc }) => desc(posts.createdAt),
       where: (posts, { eq }) => eq(posts.published, true),
@@ -12,19 +13,44 @@ export class DrizzePostRepository implements PostRepository {
     return posts;
   }
   async findBySlugPublic(slug: string): Promise<PostModel> {
-    throw new Error('Method not implemented.');
+    console.log('findBySlugPublic');
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq, and }) =>
+        and(eq(posts.published, true), eq(posts.slug, slug)),
+    });
+
+    if (!post) throw new Error('Post não encontrado para slug');
+
+    return post;
   }
   async findAll(): Promise<PostModel[]> {
-    throw new Error('Method not implemented.');
+    console.log('findAll');
+    const posts = await drizzleDb.query.posts.findMany({
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+    });
+
+    return posts;
   }
   async findById(id: string): Promise<PostModel> {
-    throw new Error('Method not implemented.');
+    console.log('findById');
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq }) => eq(posts.id, id),
+    });
+
+    if (!post) throw new Error('Post não encontrado para id');
+
+    return post;
   }
 }
 
 (async () => {
-  const repo = new DrizzePostRepository();
-  const posts = await repo.findAllPublic();
-
-  posts.forEach(post => console.log(post.excerpt, post.published));
+  //   o que for necessário. true
+  // o Next.js já vem com várias decisões prontas, permitindo que você comece a desenvolver mais rapidamente. false
+  //   9eb8b7ac-2b48-4835-880a-a1c798e1a595 true
+  // 6b204dab-2312-4525-820a-a0463560835f false
+  // const repo = new DrizzePostRepository();
+  // const posts = await repo.findAll();
+  // posts.forEach(post => console.log(post.id, post.published));
+  // const post = await repo.findBySlugPublic('o que for necessário.');
+  // console.log(post);
 })();
