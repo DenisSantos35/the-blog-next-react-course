42eb4312602d89ce58c96f163111125fb0900a47
Denis Santos <denisdiogo34@gmail.com>
2025-07-23 09:13:59 -0300

    Full Route Cache com dynamic e dynamicParams ou generateStaticParams

--- src/app/exemplo/[id]/page.tsx
+++ src/app/exemplo/[id]/page.tsx
@@ -1,10 +1,24 @@
 import { formatHour } from '@/utils/format-datetime';
 //esta pagina é dinamica, ou seja, ela é renderizada no servidor a cada requisição, mantendo a funcionalidade de dinamismo mesmo após o build.
-export default async function ExampleDynamicPage() {
+//SSG -> static side generation, gera uma pagina estatica, mas com a funcionalidade de dinamismo.
+//export const dynamic = 'force-static'; == export async function generateStaticParams() { return []; }
+export const dynamicParams = true;
+export async function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }];
+}
+export default async function ExampleDynamicPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   const hour = formatHour(Date.now());
+
   return (
     <main className='min-h-[600px] text-xl font-bold'>
-      <div>Hora: {hour}</div>
+      <div>
+        Hora: {hour} id: {id}
+      </div>
     </main>
   );
 }
--- src/app/exemplo/page.tsx
+++ src/app/exemplo/page.tsx
@@ -1,5 +1,8 @@
 import { formatHour } from '@/utils/format-datetime';
 //Está pagina quando feita em Dev ela é dinamica porem apos o build ela é estatica, e toda funcionalidade de dinamica é perdida.
+
+//para tornar este componente apos a build dinamico e preciso exportar uma variavel dynamic = force-dynamic
+export const dynamic = 'force-dynamic';
 export default async function ExamplePage() {
   const hour = formatHour(Date.now());
   return (
