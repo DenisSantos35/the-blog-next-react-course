552f072595aee38118cdc356ade314788979ef9d
Denis Santos <denisdiogo34@gmail.com>
2025-07-24 10:07:44 -0300

    ISR(Incremental Static Regeneration) com revalidação por tempo (revalidate)

--- src/app/exemplo/[id]/page.tsx
+++ src/app/exemplo/[id]/page.tsx
@@ -1,11 +1,19 @@
 import { formatHour } from '@/utils/format-datetime';
+// import { revalidatePath, revalidateTag } from 'next/cache';
 //esta pagina é dinamica, ou seja, ela é renderizada no servidor a cada requisição, mantendo a funcionalidade de dinamismo mesmo após o build.
 //SSG -> static side generation, gera uma pagina estatica, mas com a funcionalidade de dinamismo.
-//export const dynamic = 'force-static'; == export async function generateStaticParams() { return []; }
-export const dynamicParams = true;
-export async function generateStaticParams() {
-  return [{ id: '1' }, { id: '2' }];
-}
+export const dynamic = 'force-static'; // == export async function generateStaticParams() { return []; }
+export const revalidate = 10;
+//export const dynamicParams = true;
+// export async function generateStaticParams() {
+//   return [{ id: '1' }, { id: '2' }];
+// }
+
+// // exemplo de revalidação por caminho
+// revalidatePath('/exemplo/1');
+// // exemplo de revalidação por tag
+// revalidateTag('exemplo-1');
+
 export default async function ExampleDynamicPage({
   params,
 }: {
