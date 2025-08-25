797b73fac5608ad9fa85770ec2d0d70186d5bca7
Denis Santos <denisdiogo34@gmail.com>
2025-08-25 09:28:47 -0300

    Usando TypeScript para adicionar variações de estilo de componente

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -4,12 +4,15 @@ export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='py-16 text-6xl'>
-      <Button
-        className='bg-red-500 px-4 py-2 rounded-lg items-center justify-center text-white text-2xl transform hover:bg-red-600 transition-colors cursor-pointer'
-        type='submit'
-      >
-        Teste component button
+    <div className='py-16 flex gap-4 flex-wrap'>
+      <Button variant='default' size='sm'>
+        Confirmar
+      </Button>
+      <Button variant='ghost' size='md'>
+        Confirmar
+      </Button>
+      <Button variant='danger' size='lg'>
+        Confirmar
       </Button>
     </div>
   );
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,30 @@
-type ButtonProps = {} & React.ComponentProps<'button'>; //caso queira estendender componente seu ou de outra lib vc utilza typeof nome do componente
+import clsx from 'clsx';
 
-export function Button({ children, ...props }: ButtonProps) {
-  return <button {...props}>{children}</button>;
+type ButtonsVariants = 'default' | 'ghost' | 'danger';
+type ButtonSizes = 'sm' | 'md' | 'lg';
+
+type ButtonProps = {
+  variant?: ButtonsVariants;
+  size?: ButtonSizes;
+} & React.ComponentProps<'button'>; //caso queira estendender componente seu ou de outra lib vc utilza typeof nome do componente
+
+export function Button({
+  variant = 'default',
+  size = 'md',
+  ...props
+}: ButtonProps) {
+  const buttonsVariants: Record<ButtonsVariants, string> = {
+    default: clsx('bg-blue-600 text-blue-100'),
+    ghost: clsx('bg-slate-200 text-slate-900'),
+    danger: clsx('bg-red-600 text-red-100'),
+  };
+  const buttonSizes: Record<ButtonSizes, string> = {
+    sm: clsx(''),
+    md: clsx(''),
+    lg: clsx(''),
+  };
+
+  const buttonClasses = clsx(buttonsVariants[variant], buttonSizes[size]);
+
+  return <button className={buttonClasses} {...props}></button>;
 }
