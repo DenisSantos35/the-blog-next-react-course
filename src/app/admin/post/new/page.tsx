4fa503902ba91bb89b1afcdfd63d28cc9ae86379
Denis Santos <denisdiogo34@gmail.com>
2025-08-26 09:15:54 -0300

    Concluindo o Button e ajustando os botões de Dialog.

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,19 +1,42 @@
 import { Button } from '@/components/Button';
+import { BanIcon, BugIcon, CheckIcon } from 'lucide-react';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='py-16 flex gap-4 flex-wrap'>
-      <Button variant='default' size='sm'>
-        Confirmar
-      </Button>
-      <Button variant='ghost' size='md'>
-        Confirmar
-      </Button>
-      <Button variant='danger' size='lg'>
-        Confirmar
-      </Button>
+    <div>
+      <div className='py-16 flex gap-4 flex-wrap items-center'>
+        <Button variant='default' size='sm'>
+          <BugIcon /> Confirmar
+        </Button>
+        <Button variant='ghost' size='md'>
+          <BugIcon /> Confirmar
+        </Button>
+        <Button variant='danger' size='lg'>
+          <BugIcon /> Confirmar
+        </Button>
+      </div>
+      <div className='py-16 flex gap-4 flex-wrap items-center'>
+        <Button variant='default' size='sm' disabled>
+          <BugIcon /> Confirmar
+        </Button>
+        <Button variant='ghost' size='md' disabled>
+          <BugIcon /> Confirmar
+        </Button>
+        <Button variant='danger' size='lg' disabled>
+          <BugIcon /> Confirmar
+        </Button>
+        <Button variant='danger' size='lg' className='w-full'>
+          <BugIcon /> Confirmar
+        </Button>
+        <Button variant='ghost' size='lg' className='w-full'>
+          <BanIcon /> Cancelar
+        </Button>
+        <Button variant='default' size='lg' className='w-full'>
+          <CheckIcon /> OK
+        </Button>
+      </div>
     </div>
   );
 }
