3a06b0230736e0e7f1da524dd93252c524d73d48
Denis Santos <denisdiogo34@gmail.com>
2025-08-28 08:16:10 -0300

    Formulário de criação e edição do post (ManagePostForm)

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,46 +1,12 @@
-import { Button } from '@/components/Button';
-import { InputCheckbox } from '@/components/InputCheckbox';
-import { InputText } from '@/components/InputText';
+import { ManagePostForm } from '@/components/Admin/ManagePostForm';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <form action='' className='mb-16'>
-      <div className='flex flex-col gap-6'>
-        <InputCheckbox labelText='Checkbox' />
-        <InputText
-          labelText='Nome:'
-          placeholder='Digite seu nome'
-          type='password'
-        />
-        <InputText labelText='Sobrenome:' placeholder='Digite seu sobrenome' />
-        <InputText
-          disabled
-          labelText='Sobrenome:'
-          placeholder='Digite seu sobrenome'
-          defaultValue='Hellow World!'
-        />
-        <InputText
-          disabled
-          labelText='Sobrenome:'
-          placeholder='Digite seu sobrenome'
-        />
-        <InputText
-          labelText='Sobrenome:'
-          placeholder='Digite seu sobrenome'
-          readOnly
-        />
-        <InputText
-          labelText='Sobrenome:'
-          placeholder='Digite seu sobrenome'
-          defaultValue='Hellow World!'
-          readOnly
-        />
-        <div className='mt-4'>
-          <Button type='submit'>Enviar Formulario</Button>
-        </div>
-      </div>
-    </form>
+    <>
+      <h1>Criar Post</h1>
+      <ManagePostForm />
+    </>
   );
 }
---
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Button } from '@/components/Button';
+import { InputCheckbox } from '@/components/InputCheckbox';
+import { InputText } from '@/components/InputText';
+
+export function ManagePostForm() {
+  return (
+    <form action='' className='mb-16'>
+      <div className='flex flex-col gap-6'>
+        <InputCheckbox labelText='Checkbox' />
+        <InputText
+          labelText='Nome:'
+          placeholder='Digite seu nome'
+          type='password'
+        />
+        <InputText labelText='Sobrenome:' placeholder='Digite seu sobrenome' />
+        <InputText
+          disabled
+          labelText='Sobrenome:'
+          placeholder='Digite seu sobrenome'
+          defaultValue='Hellow World!'
+        />
+        <InputText
+          disabled
+          labelText='Sobrenome:'
+          placeholder='Digite seu sobrenome'
+        />
+        <InputText
+          labelText='Sobrenome:'
+          placeholder='Digite seu sobrenome'
+          readOnly
+        />
+        <InputText
+          labelText='Sobrenome:'
+          placeholder='Digite seu sobrenome'
+          defaultValue='Hellow World!'
+          readOnly
+        />
+        <div className='mt-4'>
+          <Button type='submit'>Enviar</Button>
+        </div>
+      </div>
+    </form>
+  );
+}
