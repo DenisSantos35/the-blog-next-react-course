8f441539dc8e74ff2a6d11d968ac23dc3b240ae0
Denis Santos <denisdiogo34@gmail.com>
2025-08-29 09:38:56 -0300

    ImageUploader - input(file) e botão para upload de imagens.

--- src/components/Admin/imageUploader/index.tsx
+++ src/components/Admin/imageUploader/index.tsx
@@ -1,14 +1,42 @@
 'use client';
 
 import { Button } from '@/components/Button';
+import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
 import { ImageUpIcon } from 'lucide-react';
 import { useRef } from 'react';
+import { toast } from 'react-toastify';
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
   function handleChooseFile() {
     if (!fileInputRef.current) return;
     fileInputRef.current.click();
+  }
+
+  function handleChange() {
+    toast.dismiss();
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+      const readbleMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
+      toast.error(
+        `Imagem muito grande.\n Tamanho Máximo permitido: ${readbleMaxSize} KB`,
+      );
+      fileInput.value = '';
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+    console.log(formData);
+    //TODO: Criar a action para upload de arquivo
+
+    fileInput.value = '';
   }
   return (
     <div className='flex flex-col gap-2 py-4'>
@@ -17,6 +45,7 @@ export function ImageUploader() {
         Enviar uma imagem
       </Button>
       <input
+        onChange={handleChange}
         ref={fileInputRef}
         className='hidden'
         name='file'
