de3e4b3066f488e00937fb6eb64ae92660c2c64f
Denis Santos <denisdiogo34@gmail.com>
2025-11-12 08:20:36 -0300

    Checagem de autenticação em page.tsx e layout.tsx (server components)

--- src/app/admin/post/layout.tsx
+++ src/app/admin/post/layout.tsx
@@ -1,12 +1,14 @@
 import { Menuadmin } from '@/components/Admin/MenuAdmin';
+import { requireLoginSessionOrRedirect } from '@/lib/login/manage_login';
 
 type AdminPostLayout = {
   children: React.ReactNode;
 };
 
-export default function AdminPostLayout({
+export default async function AdminPostLayout({
   children,
 }: Readonly<AdminPostLayout>) {
+  await requireLoginSessionOrRedirect();
   return (
     <>
       <Menuadmin />
